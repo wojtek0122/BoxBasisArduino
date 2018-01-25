474059155ad0256720116780ab57231367fdfa24
Wojciech Hałapup <wojtek0122@wp.pl>
2018-01-25 21:07:38 +0100

    Upload

---
+++ BoxBasisArduino/BoxBasisArduino/BoxBasisArduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BoxBasisArduino.ino
+ Created:	1/25/2018 9:05:33 PM
+ Author:	Asus
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
